4991a8b8d7c6ae0c5b9e15133b35e9799a3e47ef
Warsono <warsono16694@gmail.com>
2025-06-16 13:16:06 +0700

    feat: add shadcn-vue

---
+++ app/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+useHead({
+  title: 'Nuxt Shadcn',
+  meta: [
+    { name: 'description', content: 'A minimal Nuxt + shadcn-vue starter application' }
+  ]
+});
+
+const count = ref(0);
+</script>
+
+<template>
+    <div class="h-screen flex items-center justify-center">
+  <div class="max-w-xl mx-auto p-6 text-center">
+    <h1 class="text-3xl font-bold mb-4">Welcome to Nuxt Shadcn!</h1>
+    <p class="mb-4">
+        This is a minimal Nuxt and shadcn-vue starter application.
+    </p>
+    <Button
+      @click="count++"
+    >
+      Click me! Count: {{ count }}
+    </Button>
+  </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "shadcn-nuxt",
+  ],
+
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './app/components/ui'
+  }
+});
