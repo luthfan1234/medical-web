56ab5bcd1c9ebe5450826522fdde48497f86b54e
_umannn <134380467+luthfan1234@users.noreply.github.com>
2025-10-15 17:19:22 +0700

    fix: update blog entries in BlogsCarouselSection for consistency and improved layout

--- app/components/blogs-screen/BlogsCarouselSection.vue
+++ app/components/blogs-screen/BlogsCarouselSection.vue
@@ -11,87 +11,97 @@ import {
 const blogs = ref([
   {
     id: 1,
-    title: 'Why You Should Choose a Pediatric Dentist',
-    description:
-      'Pediatric dentists are dental specialists who focus on the oral health of children from infancy through adolescence.',
-    image: '/images/blogs/blog1.png',
+    category: 'Self Care',
+    writer: '~Anita Jackson',
+    title: 'Care of your Teeth',
+    description: ' Lorem ipsum dolor sit amet consectetur.',
+    image: '/images/article/article6.png',
     link: '#',
   },
   {
     id: 2,
-    title: 'The Importance of Oral Hygiene for Children',
-    description:
-      'Good oral hygiene is essential for maintaining healthy teeth and gums. Children should be taught proper oral hygiene habits from an early age.',
-    image: '/images/blogs/blog2.png',
+    category: 'Self Care',
+    writer: '~Anita Jackson',
+    title: 'Care of your Teeth',
+    description: ' Lorem ipsum dolor sit amet consectetur.',
+    image: '/images/article/article6.png',
     link: '#',
   },
   {
     id: 3,
-    title: 'The Benefits of Dental Sealants for Children',
-    description:
-      'Dental sealants are a thin layer of material applied to the teeth to prevent tooth decay. They are an effective way to prevent cavities from forming on the teeth.',
-    image: '/images/blogs/blog3.png',
+    category: 'Self Care',
+    writer: '~Anita Jackson',
+    title: 'Care of your Teeth',
+    description: ' Lorem ipsum dolor sit amet consectetur.',
+    image: '/images/article/article6.png',
     link: '#',
   },
   {
     id: 4,
-    title: 'Why You Should Choose a Pediatric Dentist',
-    description:
-      'Pediatric dentists are dental specialists who focus on the oral health of children from infancy through adolescence.',
-    image: '/images/blogs/blog4.png',
+    category: 'Self Care',
+    title: 'Care of your Teeth',
+    writer: '~Anita Jackson',
+    description: ' Lorem ipsum dolor sit amet consectetur.',
+    image: '/images/article/article6.png',
     link: '#',
   },
   {
     id: 5,
-    title: 'The Importance of Oral Hygiene for Children',
-    description:
-      'Good oral hygiene is essential for maintaining healthy teeth and gums. Children should be taught proper oral hygiene habits from an early age.',
-    image: '/images/blogs/blog5.png',
+    category: 'Self Care',
+    title: 'Care of your Teeth',
+    writer: '~Anita Jackson',
+    description: ' Lorem ipsum dolor sit amet consectetur.',
+    image: '/images/article/article6.png',
     link: '#',
   },
   {
     id: 6,
-    title: 'The Benefits of Dental Sealants for Children',
-    description:
-      'Dental sealants are a thin layer of material applied to the teeth to prevent tooth decay. They are an effective way to prevent cavities from forming on the teeth.',
-    image: '/images/blogs/blog6.png',
+    category: 'Self Care',
+    title: 'Care of your Teeth',
+    writer: '~Anita Jackson',
+    description: ' Lorem ipsum dolor sit amet consectetur.',
+    image: '/images/article/article6.png',
     link: '#',
   },
 ]);
 </script>
 
 <template>
-  <div
-    class="w-full flex flex-col text-center items-center justify-center bg-soft-blue"
-  >
-    <div class="w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+  <div class="w-full flex flex-col">
+    <div class="">
       <Carousel>
-        <CarouselContent class="-ml-3 mb-10">
+        <CarouselContent class="-ml-3 bg">
           <CarouselItem
             v-for="blog in blogs"
             :key="blog.id"
             class="pl-3 md:basis-1/2 lg:basis-1/3 xl:basis-1/4"
           >
             <div
-              class="relative rounded-xl overflow-hidden bg-white w-full h-82 flex flex-col"
+              class="relative rounded-xl overflow-hidden bg-soft-blue w-full h-[31rem] flex flex-col px-5 py-6"
             >
-              <div class="relative w-full h-full">
+              <div class="">
                 <img
                   :src="blog.image"
                   :alt="blog.title"
                   class="w-full h-full object-cover"
                 />
               </div>
+              <div class="flex justify-between items-center">
+                <button
+                  class="w-24 h-9 bg-deep-blue text-white rounded-lg items-start"
+                >
+                  {{ blog.category }}
+                </button>
+                <p class="text-sm text-gray-500 mt-1">{{ blog.writer }}</p>
+              </div>
 
-              <div
-                class="absolute left-3 right-3 bottom-3 sm:left-5 sm:right-5 sm:bottom-5 bg-gradient-to-r from-sky-blue to-light-gray text-left p-3 sm:p-4 rounded-lg"
-              >
-                <h3 class="text-white font-semibold text-base sm:text-lg">
+              <div>
+                <CardTitle class="font-medium text-deep-blue text-lg text-left">
                   {{ blog.title }}
-                </h3>
-                <p class="text-gray-200 text-sm">
+                </CardTitle>
+                <CardDescription class="text-gray-600 text-sm text-left">
                   {{ blog.description }}
-                </p>
+                </CardDescription>
               </div>
             </div>
           </CarouselItem>
@@ -101,10 +111,10 @@ const blogs = ref([
           class="flex gap-4 absolute -bottom-10 sm:-bottom-8 left-1/2 -translate-x-1/2 px-4 sm:px-14"
         >
           <CarouselPrevious
-            class="inline-flex items-center justify-center w-20 h-10 sm:w-24 sm:h-11 rounded-none bg-white hover:bg-gray-200 hover:text-deep-blue border-none"
+            class="inline-flex items-center justify-center w-20 h-10 sm:w-24 sm:h-11 rounded-none bg-deep-blue hover:bg-gray-400 hover:text-deep-blue text-white border-none"
           />
           <CarouselNext
-            class="inline-flex items-center justify-center w-20 h-10 sm:w-24 sm:h-11 rounded-none bg-white hover:bg-gray-200 hover:text-deep-blue border-none"
+            class="inline-flex items-center justify-center w-20 h-10 sm:w-24 sm:h-11 rounded-none bg-deep-blue hover:bg-gray-400 hover:text-deep-blue text-white border-none"
           />
         </div>
       </Carousel>
--- app/components/services-screen/ServicesPageSection.vue
+++ app/components/services-screen/ServicesPageSection.vue
@@ -51,7 +51,7 @@ const services = ref([
 </script>
 
 <template>
-  <div class="bg-soft-blue">
+  <div class="">
     <div class="flex items-center justify-center">
       <div
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8"
