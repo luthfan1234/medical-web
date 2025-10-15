44ae44879646c5e757b11acc6c26780c6d2dc66d
_umannn <134380467+luthfan1234@users.noreply.github.com>
2025-10-15 09:57:15 +0700

    feat: add services page with initial layout and components

---
+++ app/pages/services.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Footer from '~/components/Footer.vue';
+import NavigationBar from '~/components/NavigationBar.vue';
+</script>
+
+<template>
+  <div>
+    <div class="px-20 py-10 font-sans">
+      <NavigationBar />
+      <HeroSection />
+      <WelcomeSection />
+      <WhyChooseUsSection />
+      <CTASection />
+      <PlaylistSection />
+    </div>
+
+    <!-- Section tanpa padding -->
+    <SpecialistsSection />
+
+    <div class="px-20">
+      <!-- <TestimonialsSection /> -->
+      <TestimonialsSection />
+      <ArticleSection />
+      <FAQSection />
+      <PromoSection />
+      <Footer />
+    </div>
+  </div>
+</template>
